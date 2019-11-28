9b5c35fb06570a89783721eec796adeb931ffa7a
Daniel Lazarenko <daniel.lazarenko@videxio.com>
2019-11-28 15:28:53 +0100

    Fix deprecation of top/bottom layout guides on iOS 11+ projects.

--- QRCodeReaderViewController/QRCodeReaderViewController.m
+++ QRCodeReaderViewController/QRCodeReaderViewController.m
@@ -261,8 +261,8 @@
       leftLayoutAnchor = self.view.safeAreaLayoutGuide.leftAnchor;
       rightLayoutAnchor = self.view.safeAreaLayoutGuide.rightAnchor;
     } else {
-      topLayoutAnchor = self.topLayoutGuide.topAnchor;
-      bottomLayoutAnchor = self.bottomLayoutGuide.bottomAnchor;
+      topLayoutAnchor = self.view.topAnchor;
+      bottomLayoutAnchor = self.view.bottomAnchor;
       leftLayoutAnchor = self.view.leftAnchor;
       rightLayoutAnchor = self.view.rightAnchor;
     }
